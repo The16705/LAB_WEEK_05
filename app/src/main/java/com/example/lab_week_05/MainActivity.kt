16c7f392ddcca1cccebcce77c9ee9bfa15a48d2d
The16705 <theodorusfernando6705@gmail.com>
2025-09-27 13:45:46 +0700

    Commit No. 1 - Retrofit Setup and Fetch API JSON Response

--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -16,5 +16,54 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        private val retrofit by lazy{
+            Retrofit.Builder()
+                .baseUrl("https://api.thecatapi.com/v1/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build()
+        }
+
+        private val catApiService by lazy {
+            retrofit.create(CatApiService::class.java)
+        }
+
+        private val apiResponseView: TextView by lazy{
+            findViewById(R.id.api_response)
+        }
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            getCatImageResponse()
+        }
+        private fun getCatImageResponse() {
+            val call = catApiService.searchImages(1, "full")
+            call.enqueue(object: Callback<String> {
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    Log.e(MAIN_ACTIVITY, "Failed to get response", t)
+                }
+
+                override fun onResponse(call: Call<String>, response:
+                Response<String>) {
+                    if(response.isSuccessful){
+                        apiResponseView.text = response.body()
+                    }
+                    else{
+                        Log.e(MAIN_ACTIVITY, "Failed to get response\n" +
+                                response.errorBody()?.string().orEmpty()
+                        )
+                    }
+                }
+            })
+        }
+        companion object{
+            const val MAIN_ACTIVITY = "MAIN_ACTIVITY"
+        }
+
+
+
+
+
+
     }
 }
